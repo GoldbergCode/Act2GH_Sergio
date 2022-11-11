6ff749f5fbc730904f7e234f881bf9e209abdf8a
Vertedor <vertedor86@gmail.com>
2022-11-11 14:39:07 +0100

    Minor changes

--- ArrayRandom/src/ArrayRandom.java
+++ ArrayRandom/src/ArrayRandom.java
@@ -2,10 +2,10 @@
 public class ArrayRandom {
 
 	public static void main(String[] args) {
-    int[] numeros = new int[20];
+    int[] numeros = new int[30];
     for (int i = 0; i < numeros.length; i++) {
       numeros[i] = (int) ((Math.random() * 10) + 1);
-      System.out.println("[" + i + "] = " + numeros[i]);
+      System.out.println("[" + (i + 1) + "] = " + numeros[i]);
     }
   }
 }
