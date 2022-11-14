1f9cdf9ca1bf348e2eea0b00023be01c9a0424c3
EPPalom <115471500+EPPalom@users.noreply.github.com>
2022-11-14 21:54:27 +0100

    Update ArrayRandom.java

--- ArrayRandom/src/ArrayRandom.java
+++ ArrayRandom/src/ArrayRandom.java
@@ -4,7 +4,7 @@ public class ArrayRandom {
   public static void main(String[] args) {
     int[] numeros = new int[30];
     for (int i = 0; i < numeros.length; i++) {
-      numeros[i] = (int) ((Math.random() * 10) + 1);
+      numeros[i] = (int) ((Math.random()*10+1);
       System.out.println("[" + ((i + 1) + 1) + "] = " + numeros[i]);
     }
 
